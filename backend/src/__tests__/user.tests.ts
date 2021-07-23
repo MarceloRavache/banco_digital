c08143a6aa64f11fb27f6dc9b204bc79975b2723
MarceloRavache <jm.ravache@outlook.com>
2021-07-22 21:40:03 -0300

    refactor: updated testes

--- backend/src/__tests__/user.tests.ts
+++ backend/src/__tests__/user.tests.ts
@@ -3,29 +3,40 @@ import request from "supertest";
 
 jest.useFakeTimers()
 
+const testData = {
+    name: "teste",
+    email: "teste",
+    password: "1234",
+    cpf:"00000"
+}
+
 describe("Usuario", () => {
     test("Create - Caso o Nome estiver vazio retorna um erro", async ()=> {
         const result = await request(app)
         .post("/users")
-        .send({name: ''})
+        .send({...testData, name:""})
         .set('Accept', 'application/json');
-        console.log(result)
         expect(result.status).toEqual(400);
     })
     test("Create - Caso o Email estiver vazio retorna um erro", async ()=> {
         const result = await request(app)
         .post("/users")
-        .send({email: ''})
+        .send({...testData, email:""})
         .set('Accept', 'application/json');
-        console.log(result)
         expect(result.status).toEqual(400);
     })
     test("Create - Caso o Password estiver vazio retorna um erro", async ()=> {
         const result = await request(app)
         .post("/users")
-        .send({password: ''})
+        .send({...testData, password:""})
         .set('Accept', 'application/json');
-        console.log(result)
+        expect(result.status).toEqual(400);
+    })
+    test("Create - Caso o CPF estiver vazio retorna um erro", async ()=> {
+        const result = await request(app)
+        .post("/users")
+        .send({...testData, cpf:""})
+        .set('Accept', 'application/json');
         expect(result.status).toEqual(400);
     })
     test("Put - Caso o Email estiver vazio retorna um erro", async ()=> {
@@ -33,7 +44,6 @@ describe("Usuario", () => {
         .put("/users/1")
         .send({email: ''})
         .set('Accept', 'application/json');
-        console.log(result)
         expect(result.status).toEqual(400);
     })
     test("Put - Caso o Usuario não existir retorna um erro", async ()=> {
@@ -41,7 +51,6 @@ describe("Usuario", () => {
         .put("/users/0")
         .send({email: 'teste@teste.com'})
         .set('Accept', 'application/json');
-        console.log(result)
-        expect(result.status).toEqual(400);
+        expect(result.status).toEqual(404);
     })
 })
